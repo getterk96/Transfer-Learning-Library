9d78df5bda071a51c5c7a5238b31008c6cba7467
getterk96 <getterk@163.com>
2021-02-24 14:44:57 +0800

    ADD: New method

--- examples-da/segmentation/advent.sh
+++ examples-da/segmentation/advent.sh
@@ -1,4 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python advent.py data/GTA5 data/Cityscapes -s GTA5 -t Cityscapes \
-    --log logs/advent/gtav2cityscapes
-CUDA_VISIBLE_DEVICES=0 python advent.py data/synthia data/Cityscapes -s Synthia -t Cityscapes \
-    --log logs/advent/synthia2cityscapes
+CUDA_VISIBLE_DEVICES=1 python advent.py /data/GTA-5 /data/cityscapes -s GTA5 -t Cityscapes --log logs/advent/gtav2cityscapes_test
+#CUDA_VISIBLE_DEVICES=0 python advent.py data/synthia data/Cityscapes -s Synthia -t Cityscapes \
+#    --log logs/advent/synthia2cityscapes
